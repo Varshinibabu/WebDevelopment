4f01036019b4a457ba80e04bebf2f3cc130b397c
anandbabu2405@gmail.com <anandbabu2405@gmail.com>
2023-06-02 21:17:19 +0530

    section three

--- Program.cs
+++ Program.cs
@@ -6,13 +6,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 builder.Services.AddDbContext<DataContext>(opt=>
 {
               opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 var app = builder.Build();
-
-
+//configure the http request pipeline
+app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.MapControllers();
 
 app.Run();
